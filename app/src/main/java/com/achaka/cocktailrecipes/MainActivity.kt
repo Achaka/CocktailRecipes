2c098ec3a374c8b2a17ec470931b3c0318184486
Achaka <oen-feedback@yandex.ru>
2021-11-21 00:53:30 +0300

    bottom navigation view with colors

--- app/src/main/java/com/achaka/cocktailrecipes/MainActivity.kt
+++ app/src/main/java/com/achaka/cocktailrecipes/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_CocktailRecipes)
+
+
         setContentView(R.layout.activity_main)
     }
 }
