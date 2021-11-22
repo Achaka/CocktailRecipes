2e1b501e940174773ed43edcd4d8e398d08e754e
Achaka2 <oen.feedback@gmail.com>
2021-11-22 13:20:03 +0300

    network wip: retrofit constants and values setup

--- app/src/main/java/com/achaka/cocktailrecipes/model/network/NetworkServiceApi.kt
+++ app/src/main/java/com/achaka/cocktailrecipes/model/network/NetworkServiceApi.kt
@@ -1,20 +1,47 @@
 package com.achaka.cocktailrecipes.model.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-private const val baseUrl = ""
-private const val header = ""
-private const val authToken = ""
+private const val BASE_URL = "https://the-cocktail-db.p.rapidapi.com/"
+private const val HOST = "x-rapidapi-host: the-cocktail-db.p.rapidapi.com"
+private const val API_KEY = "x-rapidapi-key: e3a70448e3mshd434aadb6650e4ap12b34bjsn710a0f23a9ac"
+
+val moshi = Moshi.Builder()
+    .add(RxJava3CallAdapterFactory.create())
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+val loggingInterceptor = HttpLoggingInterceptor()
+
+val client = OkHttpClient().newBuilder()
+    .addInterceptor(
+        loggingInterceptor.setLevel(
+            HttpLoggingInterceptor.Level.BODY
+        )
+    )
+    .build()
 
 val retrofit = Retrofit.Builder()
-    .
+    .baseUrl(BASE_URL)
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+    .client(client)
+    .build()
 
 interface NetworkServiceApi {
+
+    //
 
 }
 
 object NetworkApi {
     val retrofitService: NetworkServiceApi by lazy {
-        retrofit.
+        retrofit.create(NetworkServiceApi::class.java)
     }
 }
