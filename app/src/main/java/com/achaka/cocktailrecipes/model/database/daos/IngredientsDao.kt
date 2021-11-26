79beb18370f0dd9c014689381d6f1e3b1222f40c
Achaka2 <oen.feedback@gmail.com>
2021-11-26 19:12:55 +0300

    wip: database daos basic implementation

--- app/src/main/java/com/achaka/cocktailrecipes/model/database/daos/IngredientsDao.kt
+++ app/src/main/java/com/achaka/cocktailrecipes/model/database/daos/IngredientsDao.kt
@@ -2,21 +2,20 @@ package com.achaka.cocktailrecipes.model.database.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.achaka.cocktailrecipes.model.domain.Ingredient
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface IngredientsDao {
 
     @Insert
-    fun insertIngredient(ingredient: Ingredient) {
+    fun insertIngredient(ingredient: Ingredient)
 
-    }
+    @Query("SELECT * FROM databaseingredient WHERE id=:ingredientId")
+    fun getIngredientById(ingredientId: Int): Flow<Ingredient>
 
-    fun getIngredient() {
-
-    }
-
-    fun getIngredientsList() {
-
-    }
+    @Query("SELECT * FROM databaseingredient WHERE name=:ingredientName")
+    fun getIngredientByName(ingredientName: String): Flow<Ingredient>
 }
